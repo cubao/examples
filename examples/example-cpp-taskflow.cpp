6fcac8c295152573bb0ed198211f19ec25370946
TANG ZHIXIONG <zhixiong@momenta.works>
2019-06-14 08:13:01 +0800

    json works, task-flow needs C++17

--- examples/example-cpp-taskflow.cpp
+++ examples/example-cpp-taskflow.cpp
@@ -1,7 +1,35 @@
-#include "utils.hpp"
+#include <iostream>
 
-int main(int argc, char **argv)
+int main()
 {
-    hello();
+    std::cout << "C++17 needed" << std::endl;
     return 0;
 }
+
+// #include <taskflow/taskflow.hpp>
+//
+// int main(){
+//
+//     tf::Taskflow tf;
+//
+//     auto [A, B, C, D] = tf.emplace(
+//             [] () { std::cout << "TaskA\n"; },               //  task
+//             dependency graph
+//             [] () { std::cout << "TaskB\n"; },               //
+//             [] () { std::cout << "TaskC\n"; },               // +---+
+//             [] () { std::cout << "TaskD\n"; }                //    +---->| B
+//             |-----+
+//             );                                                 //    |
+//             +---+     |
+//     //  +---+           +-v-+
+//     A.precede(B);  // A runs before B                  //  | A |           |
+//     D |
+//     A.precede(C);  // A runs before C                  //  +---+ +-^-+
+//     B.precede(D);  // B runs before D                  //    |     +---+ |
+//     C.precede(D);  // C runs before D                  //    +---->| C
+//     |-----+
+//     //          +---+
+//     tf::Executor().run(tf);  // create an executor to run the taskflow
+//
+//     return 0;
+// }
--- examples/example-json.cpp
+++ examples/example-json.cpp
@@ -1,7 +1,11 @@
-#include "utils.hpp"
+#include <iostream>
+#include <nlohmann/json.hpp>
 
-int main(int argc, char **argv)
+using json = nlohmann::json;
+
+int main()
 {
-    hello();
+    auto j = json::parse("{ \"happy\": true, \"pi\": 3.141 }");
+    std::cout << j.dump(4) << std::endl;
     return 0;
 }
