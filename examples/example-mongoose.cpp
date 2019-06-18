e9e4454585acb6db31d8b0eb5f624cf57cf10263
TANG ZHIXIONG <zhixiong@momenta.works>
2019-06-19 02:18:23 +0800

    mongoose works

--- examples/example-mongoose.cpp
+++ examples/example-mongoose.cpp
@@ -1,7 +1,34 @@
-#include "utils.hpp"
+#include "mongoose.h"
 
-int main(int argc, char **argv)
+void ev_handler(struct mg_connection *nc, int ev, void *p)
 {
-    hello();
+    struct mbuf *io = &nc->recv_mbuf;
+    (void)p;
+
+    switch (ev) {
+    case MG_EV_RECV:
+        mg_send(nc, io->buf, io->len); // Echo message back
+        mbuf_remove(io, io->len);      // Discard message from recv buffer
+        break;
+    default:
+        break;
+    }
+}
+
+int main(void)
+{
+    struct mg_mgr mgr;
+    const char *port1 = "1234", *port2 = "127.0.0.1:17000";
+
+    mg_mgr_init(&mgr, NULL);
+    mg_bind(&mgr, port1, ev_handler);
+    mg_bind(&mgr, port2, ev_handler);
+
+    printf("Starting echo mgr on ports %s, %s\n", port1, port2);
+    for (;;) {
+        mg_mgr_poll(&mgr, 1000);
+    }
+    mg_mgr_free(&mgr);
+
     return 0;
 }
